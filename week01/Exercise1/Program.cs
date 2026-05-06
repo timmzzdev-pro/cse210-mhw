b36479d843c3457cb8893eea4c44d7cff170dc5e
timmzzdev-progit config --global user.email tolajide1@byupathway.edu <timilehinolajide8464@gmail.com>
2026-05-06 13:53:38 +0100

    my name input

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("what is your first name? ");
+        string first = Console.ReadLine();
+        
+        Console.Write("what is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"your name is {last}, {first} {last}");
+
     }
 }
