22e499158e205be26fa9a8b5d9b6d1a8cb3735f8
timmzzdev-progit config --global user.email tolajide1@byupathway.edu <timilehinolajide8464@gmail.com>
2026-06-03 10:47:37 +0100

    all files updated

---
+++ week04/OnlineOrdering/order.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+
+public class Order
+{
+    private List<Product> _products;
+    private Customer _customer;
+
+    public Order(Customer customer)
+    {
+        _customer = customer;
+        _products = new List<Product>();
+    }
+
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
+
+    public double CalculateTotalCost()
+    {
+        double total = 0;
+
+        foreach (Product product in _products)
+        {
+            total += product.GetTotalCost();
+        }
+
+        if (_customer.LivesInUSA())
+        {
+            total += 5;
+        }
+        else
+        {
+            total += 35;
+        }
+
+        return total;
+    }
+
+    public string GetPackingLabel()
+    {
+        string label = "";
+
+        foreach (Product product in _products)
+        {
+            label += product.GetPackingInfo() + "\n";
+        }
+
+        return label;
+    }
+
+    public string GetShippingLabel()
+    {
+        return $"{_customer.GetName()}\n{_customer.GetAddress().GetFullAddress()}";
+    }
+}
